6fecbb6b78094c7cc85ca5345aa9f103dd52970e
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2022-06-15 12:17:07 +0600

    Add files via upload

---
+++ Codeforces/A. Marathon/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,n,i,sum=0;
+
+    cin>>n;
+    for (i=0; i<n; i++)
+    {
+        cin>>a;
+
+        for (j=0; j<a; j++)
+    {
+        cin>>b[j];
+    }
+
+    if (b[j])
+
+        cout<<sum<<"\n";
+        sum=0;
+    }
+
+    return 0;
+}
