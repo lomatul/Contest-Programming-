2e5680ba0648434e10644000befb52b5b5ebd950
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2022-07-13 11:34:34 +0600

    Add files via upload

---
+++ Codeforces/A. YES or YES/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    string a;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        cin>>a;
+
+
+
+        if(a== "YES" || a=="yES" || a=="YEs" || a== "YeS" || a=="yEs" || a=="yeS" || a== "Yes" ||a=="yes" )
+
+        {
+
+                {
+                     cout << "YES" << endl;
+                }
+        }
+        else
+                {
+                     cout << "NO" << endl;
+                }
+
+    }
+
+
+    return 0;
+}
