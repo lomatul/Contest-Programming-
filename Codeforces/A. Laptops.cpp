29407d31a534394e8d4069266c41566ed4a47384
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2022-11-14 11:30:03 +0600

    problem_solving

---
+++ Codeforces/A. Laptops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    long long int t,j,i,a,b,sum=0;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>a>>b;
+        
+      if (a != b) {
+            cout << "Happy Alex";
+            return 0;
+        }
+    }
+    
+       cout<<"Poor Alex"<<endl;
+
+
+    return 0;
+}
