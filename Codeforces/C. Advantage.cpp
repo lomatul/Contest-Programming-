07e2e723b33aa8abf057b95fac21c0ff0aa8992f
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2022-11-22 18:50:25 +0600

    Codeforces Round #833 div 2

---
+++ Codeforces/C. Advantage.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    long long int t,i,j,n,d;
+    string s;
+    cin>>t;
+ 
+    while(t--)
+    {
+        cin>>n;
+        long long int a[n],b[n];
+ 
+        for(i=0; i<n; i++)
+        {
+            cin>>a[i];
+            b[i]=a[i];
+        }
+ 
+        sort(b,b+n);
+ 
+        d= b[n-1];
+        j = b[n-2];
+ 
+        for(i=0; i<n; i++)
+        {
+            if(a[i]==d)
+            {
+                cout<<d-j<<" ";
+            }
+            else
+            {
+                cout<<a[i]-d<<" ";
+            }
+        }
+         cout<<"\n";
+ 
+    }
+    return 0;
+}
