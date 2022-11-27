4c024517e795f827475245497c5ff34a10430e54
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2022-11-27 21:30:46 +0600

    Problem_Solving

---
+++ Codeforces/A. Special Permutation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<queue>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    long long int t,a,i,n,b,m=1,sum=0;
+    cin>>t;
+ 
+    while(t--)
+    {
+        cin>>n;
+ 
+        for(i=2; i<=n; i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<1<<endl;
+    }
+ 
+    return 0;
+}
